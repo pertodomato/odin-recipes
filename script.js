35bdf56fb4781f87940858efe118941cc6cf9fc6
pertodomato <guilhermebeccato@gmail.com>
2023-11-15 14:45:28 -0300

    Adicionando

---
+++ script.js
@@ -0,0 +1,7 @@
+function redirecionar() {
+    var valorInput = document.getElementById("searchInput").value;
+
+    if (valorInput === "fsociety") {
+        window.location.href = "https://www.polichain.xyz";
+    }
+}
